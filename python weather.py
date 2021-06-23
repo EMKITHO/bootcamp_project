6fa0888a619cb175e5ff552a8c7cd864ae2b15e2
EMKITHO <85948322+EMKITHO@users.noreply.github.com>
2021-06-23 16:43:49 +0530

    bootcamp project is uploaded

---
+++ python weather.py
@@ -0,0 +1,26 @@
+import requests
+
+from datetime import datetime
+
+api_key = '078a0aab44d3318d124a471275cf11c6'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+with open("weather.txt", "w") as f:
+    print ("-------------------------------------------------------------",file=f)
+    print ("Weather Stats for - {}  || {}".format(location.upper(), date_time), file=f)
+    print ("-------------------------------------------------------------" ,file=f)
+
+    print ("Current temperature is: {:.2f} deg C".format(temp_city) ,file=f)
+    print ("Current weather desc  :",weather_desc, file=f)
+    print ("Current Humidity      :",hmdt, '%' , file=f)
+    print ("Current wind speed    :",wind_spd ,'kmph' ,file=f)
